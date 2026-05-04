e894a1bb9a524e7507ea0ac35eca3210831e1f06
ChurchHEllla <openai361@gmail.com>
2026-05-05 01:21:00 +0300

    quick fixes

--- src/data/sidebar/journal_info.ts
+++ src/data/sidebar/journal_info.ts
@@ -7,7 +7,7 @@ export const journal_info: MenuItem = {
     children: [
         {id: 'back-to-basics', label: 'Старый сайт', href: 'https://lib.madi.ru/nitdo/index.html'},
         {id: 'about-journal', label: 'О журнале', href: '/journals/about-journal'},
-        {id: 'editorial-board', label: 'Редакционная коллегия', href: '/journals/editorial'},
+        {id: 'editorial-board', label: 'Редакционная коллегия', href: '/journals/editorial-board'},
         {id: 'bibliometrics', label: 'Библиометрия редколлегии', href: '/journals/bibliometrics'},
         {id: 'address', label: 'Адрес редакции', href: '/journals/address'},
         {id: 'rinc-rating', label: 'Рейтинг журнала в РИНЦ', href: '/journals/rinc-rating'},
