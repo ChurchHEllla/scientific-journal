b4445ce6a478a52c93412686a0b98c3576416405
ChurchHEllla <openai361@gmail.com>
2026-05-11 02:39:51 +0300

    большая часть страниц заполнена тестовыми данными

--- src/app/info/address/page.tsx
+++ src/app/info/address/page.tsx
@@ -1,32 +1,45 @@
 import styles from "./page.module.css"
+import {StaffMember} from "@/models/address"
+import {addressData} from "@/data/address"
 export default function AddressPage() {
-
     return (
         <div>
             <table className={styles.tableBordered}>
                 <thead>
-                    <tr style={{ border: '1px solid #ddd' }}>
-                        <th>Должность</th>
-                        <th>ФИО</th>
-                        <th>Кабинет</th>
-                        <th>E-Mail</th>
-                        <th>Телефон</th>
-                    </tr>
+                <tr style={{ border: '1px solid #ddd' }}>
+                    <th>Должность</th>
+                    <th>ФИО</th>
+                    <th>Кабинет</th>
+                    <th>E-Mail</th>
+                    <th>Телефон</th>
+                </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td >Заместитель начальника редакционно-издательского отдела</td>
-                        <td>САФИНА Лилиана Михайловна</td>
-                        <td>815н</td>
-                        <td><a className={styles.link} href={'mailto:' + 'vestnik@madi.ru'}>vestnik@madi.ru</a></td>
-                        <td>8 (499) 346-01-68 доб. 3152</td>
+                {/* 3. Рендерим список сотрудников через map */}
+                {addressData.people.map((person: StaffMember, index) => (
+                    <tr key={index}>
+                        <td>{person.position}</td>
+                        <td>{person.fullName}</td>
+                        <td>{person.roomNumber}</td>
+                        <td>
+                            <a className={styles.link} href={`mailto:${person.email}`}>
+                                {person.email}
+                            </a>
+                        </td>
+                        <td>{person.phone}</td>
                     </tr>
+                ))}
                 </tbody>
             </table>
-            <br/>
-            <p>
-                125319, Москва, Ленинградский проспект, 64, комн. 815н.
-            </p>
+
+            <br />
+
+            <p>{addressData.address}</p>
+
+            {/* 4. Ссылка на сайт */}
+            <a className={styles.link} href={addressData.url} target="_blank" rel="noopener noreferrer">
+                Перейти на сайт МАДИ
+            </a>
         </div>
     )
 }
