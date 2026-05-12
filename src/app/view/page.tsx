c4d939f97fa9438e34bc4fc97cb9eee0a0a20e11
ChurchHEllla <openai361@gmail.com>
2026-05-13 00:28:08 +0300

    quick fix

--- src/app/view/page.tsx
+++ src/app/view/page.tsx
@@ -1,14 +1,8 @@
-'use client'
-
-import {useRouter, useSearchParams} from "next/navigation";
 import DocxViewer from "@components/DocxViewer";
-import styles from "@/shared/styles/styles.module.css"
-export default function ViewPage() {
-    const query = useSearchParams();
-    const router = useRouter();
+import {SearchParams} from "next/dist/server/request/search-params";
+export default async function ViewPage({searchParams}: {searchParams: SearchParams}) {
 
-    const file = query.get("file");
-
+    const file = (await searchParams).file
     if (!file) {
         return (
             <div>Файл не найден</div>
@@ -16,12 +10,6 @@ export default function ViewPage() {
     }
     return (
         <div style={{ width: "100%", display: "flex", flexDirection: "column" }}>
-            <a className={styles.link}
-               href={'#'}
-               onClick={(e) => {
-                e.preventDefault();
-                router.back();
-            }}></a>
             <div style={{display: "flex", justifyContent: "center"}}>
                 <DocxViewer fileUrl={'/assets/'+ file} withWrapper={false}/>
             </div>
